acced843cb43b8815e2fce7e6ff4eeeb58d7422f
TANNA TECHBIZ LLP <30870024+tannatechbiz@users.noreply.github.com>
2020-09-30 18:38:13 +0530

    Update tfmini1.py

--- tfmini1.py
+++ tfmini1.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*
 import serial
-import time
-ser = serial.Serial("/dev/ttyTHS1", 115200)
+
+ser = serial.Serial("/dev/ttyS0", 115200)
+
 def getTFminiData():
-while True:
-#time.sleep(0.1)
-count = ser.in_waiting
-if count > 8:
-recv = ser.read(9)
-ser.reset_input_buffer()
-# type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-# type(recv[0]), 'str' in python2, 'int' in python3
-if recv[0] == 0x59 and recv[1] == 0x59:
-distance = recv[2] + recv[3] * 256
-strength = recv[4] + recv[5] * 256
-print('(', distance, ',', strength, ')')
-ser.reset_input_buffer()
-#python3
-if recv[0] == 'Y' and recv[1] == 'Y': #python2
-lowD = int(recv[2].encode('hex'), 16)
-highD = int(recv[3].encode('hex'), 16)
-lowS = int(recv[4].encode('hex'), 16)
-highS = int(recv[5].encode('hex'), 16)
-distance = lowD + highD * 256
-strength = lowS + highS * 256
-print(distance, strength)
+    while True:
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)
+            ser.reset_input_buffer()
+            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59 is 'Y'
+                low = int(recv[2].encode('hex'), 16)
+                high = int(recv[3].encode('hex'), 16)
+                distance = low + high * 256
+                print(distance)
+                
+
+
 if __name__ == '__main__':
-try:
-if ser.is_open == False:
-ser.open()
-getTFminiData()
-except KeyboardInterrupt: # Ctrl+C
-if ser != None:
-ser.close()
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
